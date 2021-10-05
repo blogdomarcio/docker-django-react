2b9928caec6f2343dce2fb296ffd3d8a58e818b6
Marcio <blogdomarcio@live.com>
2021-10-05 00:36:06 -0300

    ST

--- django/core/settings.py
+++ django/core/settings.py
@@ -159,6 +159,6 @@ CORS_ALLOWED_ORIGINS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '/django/static/')
 STATIC_URL = '/static/'
 
