8670d9d2bdf157306a902087779c8df5e7d5f854
Marcio <blogdomarcio@live.com>
2021-10-05 00:52:03 -0300

    add css

--- django/core/settings.py
+++ django/core/settings.py
@@ -168,4 +168,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "/django/static/")
